c4d7a8fdbf26ba6e1808589df586d18b4bac637e
Maksym Kintor <kintormaksim@gmail.com>
2022-12-16 19:57:17 +0100

    final

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ async function timer(){
 	green = date.getMinutes();
 	blue = date.getSeconds();
 
-	if(blue < 10){
-		blue="0"+blue;
-	}
+	if(red < 10) red="0"+red;
+	if(green < 10) green="0"+green;
+	if(blue < 10) blue="0"+blue;
 
 	document.getElementById("time").innerHTML = "#"+red+""+green+""+blue;
 	document.getElementById("body").style.backgroundColor = '#'+red+""+green+""+blue;
